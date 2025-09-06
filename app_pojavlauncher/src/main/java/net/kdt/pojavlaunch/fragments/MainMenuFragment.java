d5ff5fe1a733d29b139fcb70b7f1c279792540ea
RaydanOMGr <andreykelbler@outlook.de>
2025-09-06 12:51:53 +0200

    Added logcat dump button

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
@@ -5,6 +5,7 @@ import static net.kdt.pojavlaunch.Tools.shareLog;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -24,7 +25,16 @@ import net.kdt.pojavlaunch.extra.ExtraCore;
 import net.kdt.pojavlaunch.instances.InstanceManager;
 import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainMenuFragment extends Fragment {
     public static final String TAG = "MainMenuFragment";
@@ -43,6 +53,7 @@ public class MainMenuFragment extends Fragment {
         Button mInstallJarButton = view.findViewById(R.id.install_jar_button);
         Button mShareLogsButton = view.findViewById(R.id.share_logs_button);
         Button mOpenDirectoryButton = view.findViewById(R.id.open_files_button);
+        Button mDumpLogcatButton = view.findViewById(R.id.dump_logcat_button);
 
         ImageButton mEditProfileButton = view.findViewById(R.id.edit_profile_button);
         Button mPlayButton = view.findViewById(R.id.play_button);
@@ -64,6 +75,39 @@ public class MainMenuFragment extends Fragment {
 
         mOpenDirectoryButton.setOnClickListener((v)-> openPath(v.getContext(), getCurrentProfileDirectory(), false));
 
+        mDumpLogcatButton.setOnClickListener((v) -> {
+            Toast.makeText(requireContext(), "Dumping logcat...", Toast.LENGTH_SHORT).show();
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
+            String date = sdf.format(new Date());
+            File logcat = new File(Tools.DIR_GAME_HOME, "logcat-dump-" + date + ".txt");
+            try {
+                Process process = Runtime.getRuntime().exec("logcat -d");
+                new Thread(() -> {
+                    try(OutputStream out = new FileOutputStream(logcat)) {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            out.write(line.getBytes(StandardCharsets.UTF_8));
+                            out.write('\n');
+                        }
+
+                        reader.close();
+                        process.waitFor();
+
+                        Tools.runOnUiThread(() -> {
+                            Toast.makeText(requireContext(), "Dumped logcat to " + logcat.getName(), Toast.LENGTH_SHORT).show();
+                        });
+                    } catch (IOException | InterruptedException e) {
+                        Toast.makeText(requireContext(), "Failed to dump logcat!", Toast.LENGTH_SHORT).show();
+                        Log.e("LogcatDumper", "Failed to dump logcat: ", e);
+                    }
+                }).start();
+            } catch (IOException e) {
+                Toast.makeText(requireContext(), "Failed to dump logcat!", Toast.LENGTH_SHORT).show();
+                Log.e("LogcatDumper", "Failed to dump logcat: ", e);
+            }
+        });
 
         mNewsButton.setOnLongClickListener((v)->{
             Tools.swapFragment(requireActivity(), GamepadMapperFragment.class, GamepadMapperFragment.TAG, null);
